8aa95ef675101bff639c2d4c10236f6205d7575d
Tan <doriantan7@gmail.com>
2018-11-30 16:36:21 +0100

    InfoChangerWithChoose

--- Assets/Script/Manager/Shop.cs
+++ Assets/Script/Manager/Shop.cs
@@ -8,7 +8,11 @@ public class Shop : MonoBehaviour
     public So_Turret GorgonTurret;
     public So_Turret GoblinTurret;
     public So_Turret SkeletonTurret;
-
+    public GameObject InfoDemon;
+    public GameObject InfoGorgon;
+    public GameObject InfoGoblin;
+    public GameObject InfoSkeleton;
+    private GameObject InfoActive;
 
     BuildManager buildManager;
 
@@ -21,20 +25,36 @@ public class Shop : MonoBehaviour
 	{
 	    Debug.Log("Demon Purchase");
         buildManager.SelectTurretToBuild(DemonTurret);
+	    InfoSkeleton.SetActive(false);
+	    InfoGorgon.SetActive(false);
+	    InfoGoblin.SetActive(false);
+        InfoDemon.SetActive(true);
 	}
     public void SelectGorgon()
     {
         Debug.Log("Gorgon Purchase");
         buildManager.SelectTurretToBuild(GorgonTurret);
+        InfoDemon.SetActive(false);
+        InfoSkeleton.SetActive(false);
+        InfoGoblin.SetActive(false);
+        InfoGorgon.SetActive(true);
     }
     public void SelectGoblin()
     {
         Debug.Log("Goblin Purchase");
         buildManager.SelectTurretToBuild(GoblinTurret);
+        InfoDemon.SetActive(false);
+        InfoSkeleton.SetActive(false);
+        InfoGorgon.SetActive(false);
+        InfoGoblin.SetActive(true);
     }
     public void SelectSkeleton()
     {
         Debug.Log("Skeleton Purchase");
         buildManager.SelectTurretToBuild(SkeletonTurret);
+        InfoDemon.SetActive(false);
+        InfoGoblin.SetActive(false);
+        InfoGorgon.SetActive(false);
+        InfoSkeleton.SetActive(true);
     }
 }
---
+++ Assets/Script/TurretInformations.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretInformations : MonoBehaviour
+{
+
+    public GameObject InfoActive;
+
+    void OnEnable()
+    {
+        
+    }
+}
